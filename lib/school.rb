c46f7be0b1efc3a28b665788b0216e577016365e
Robert Tate <btate712@gmail.com>
2019-03-11 01:49:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  @roster = {}
+  
+  def initialize(name)
+    @name = name
+  end 
+
+  def add_student(student_name, grade)
+    if !roster.include?(grade)
+      roster[grade] = []
+    end 
+    roster[grade] << student_name
+  end
+    
